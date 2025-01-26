228f3b0730ded1b22a8ad91e5c7ef63d52df6dd6
farahsamad <farah_samad@outlook.com>
2025-01-26 23:47:12 +0200

    update eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,15 +9,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
-
-// const compat = new FlatCompat({
-//   // import.meta.dirname is available after Node.js v20.11.0
-//   baseDirectory: import.meta.dirname,
-// });
-
 const eslintConfig = [
   ...compat.config({
     extends: ["next"],
